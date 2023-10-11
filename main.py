f5fd0e14c97d9026b50790cf297edcbeebee35b4
Anzhelika Kurnikova <32419743+drnaida@users.noreply.github.com>
2023-10-11 17:31:46 -0400

    feat: update starter file

--- main.py
+++ main.py
@@ -20,7 +20,15 @@ class SnakeGameClass():
 
     def update(self, imgMain, currentSnakeHead):
         if self.gameOver:
-            pass
+            cv2.putText(
+                imgMain,
+                "Game Over",
+                [50, 80],
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                fontScale=3,
+                thickness=5,
+                color=(0, 100, 0)
+            )
         else:
             pass
             # recalculate the snake
@@ -28,14 +36,27 @@ class SnakeGameClass():
             # make snake have a length max
 
             # eat food
-            # if rx - self.wFood // 2 < cx < rx + self.wFood // 2 and \
-            #        ry - self.hFood // 2 < cy < ry + self.hFood // 2:
+            #if rx - self.wApple // 2 < cx < rx + self.wApple // 2 and \
+            #        ry - self.hApple // 2 < cy < ry + self.hApple // 2:
 
             # draw snake
 
             # draw food
 
             # Check for collision
+        #     pts = np.array(self.pointsCoord[:-2], np.int32)
+        #     pts = pts.reshape((-1, 1, 2))
+        #     cv2.polylines(imgMain, [pts], False, (0, 200, 0), 3)
+        #     minDist = cv2.pointPolygonTest(pts, (cx, cy), True)
+        #     if -1 <= minDist <= 1:
+        #         self.gameOver = True
+        #         self.pointsCoord = []
+        #         self.pointsDistances = []
+        #         self.currentLength = 0
+        #         self.allowedLength = 150
+        #         self.previousHeadCoord = 0, 0
+        #         self.spawnAppleRandomly()
+        # return imgMain
 
 
 # initialize the game
