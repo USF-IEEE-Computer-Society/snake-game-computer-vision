a0549120a7e6cfd05f3f079fef034bf62d6fe08b
Anzhelika Kurnikova <32419743+drnaida@users.noreply.github.com>
2023-10-11 16:21:18 -0400

    feat: add initial file

---
+++ main.py
@@ -0,0 +1,54 @@
+import cvzone
+import cv2
+import numpy as np
+import math
+from cvzone.HandTrackingModule import HandDetector
+import random
+
+# set up video stream
+
+# initialize detector
+
+class SnakeGameClass():
+    def __init__(self, appleImagePath):
+        # initialize the snake
+        self.gameOver = False
+
+    def spawnAppleRandomly(self):
+        # spawnApplyRandomly
+        pass
+
+    def update(self, imgMain, currentSnakeHead):
+        if self.gameOver:
+            pass
+        else:
+            pass
+            # recalculate the snake
+
+            # make snake have a length max
+
+            # eat food
+            # if rx - self.wFood // 2 < cx < rx + self.wFood // 2 and \
+            #        ry - self.hFood // 2 < cy < ry + self.hFood // 2:
+
+            # draw snake
+
+            # draw food
+
+            # Check for collision
+
+
+# initialize the game
+game = SnakeGameClass("apple.png")
+
+while True:
+    pass
+    # read the videostream
+
+    # identify hands
+
+    # display video
+
+    # wait for one second
+
+img.release()
